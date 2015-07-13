76fcf15906392b799c9211d6b5106f312d379438
penge <ppenge@gmail.com>
2015-07-13 11:42:46 +0200

    Add my-links component

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('economic');
+    return Ember.Object.create({
+      economics: this.store.findAll('economic'),
+      links: [
+        {
+          title: 'myCareer',
+          url: 'http://mycareer.merck.com/',
+        },
+        {
+          title: 'Merck News (Yahoo)',
+          url: 'http://finance.yahoo.com/q?s=MRK'
+        },
+      ],
+    });
   }
 });
